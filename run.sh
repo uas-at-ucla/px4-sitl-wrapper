af8bc5f3aabfd90e5002440f37854e252c1a8bd4
Ryan Nemiroff <ryguyn@gmail.com>
2020-04-15 20:47:25 -0700

    Download and build PX4 sitl in Docker

--- run.sh
+++ run.sh
@@ -19,40 +19,32 @@ ACTION=$1
 FRAME_TYPE=$2
 LOCATION=$3
 
-PX4_FIRMWARE_PATH="./Firmware"
 DOCKER_IP="192.168.3.20" # drone's docker network ip
-
-# Helper functions.
-function check-and-reinit-submodules {
-    if git submodule status | egrep -q '^[-]|^[+]'
-    then
-            echo "Need to initialize git submodules"
-            git submodule update --init --recursive
-    fi
-}
 
 unset GAZEBO_MODE
 if [ "$FRAME_TYPE" = "" ] || [ "$FRAME_TYPE" = "quad" ]
 then
-  GAZEBO_MODE="gazebo"
+  GAZEBO_MODE="none"
 elif [ "$FRAME_TYPE" = "plane" ]
 then
-  GAZEBO_MODE="gazebo_plane"
+  GAZEBO_MODE="plane"
 else
   echo "Unknown frame type: $FRAME_TYPE"
   exit 1
 fi
 
+SITL_RUN_CMD="cd /Firmware/build/px4_sitl_default/tmp && /Firmware/Tools/sitl_run.sh /Firmware/build/px4_sitl_default/bin/px4 none gazebo $GAZEBO_MODE /Firmware /Firmware/build/px4_sitl_default"
+
 # Determine what action to perform.
 if [ "$ACTION" = "build" ]
 then
-  MAKE_CMD="make px4_sitl_default"
+  RUN_CMD="echo \"Docker container built and ran successfully\""
 elif [ "$ACTION" = "simulate_headless" ]
 then
-  MAKE_CMD="HEADLESS=1 make px4_sitl_default $GAZEBO_MODE"
+  RUN_CMD="HEADLESS=1 $SITL_RUN_CMD"
 elif [ "$ACTION" = "simulate" ]
 then
-  MAKE_CMD="make px4_sitl_default $GAZEBO_MODE"
+  RUN_CMD="$SITL_RUN_CMD"
 elif [ "$ACTION" = "mavlink_router" ]
 then
   while true
@@ -62,7 +54,7 @@ then
     do
       PX4_RUNNING_CONTAINER=$(docker ps \
         --filter status=running \
-        --filter name="uas-at-ucla_px4-simulator" \
+        --filter name="uasatucla_px4-simulator" \
         --format "{{.ID}}" \
         --latest
       )
@@ -73,7 +65,7 @@ then
     echo "PX4 simulator docker started!"
 
     docker exec -it $PX4_RUNNING_CONTAINER \
-      su - user bash -c "
+      bash -c "
       set -e
       mavlink-routerd -e \$HOST_IP:9010 -e $DOCKER_IP:9011 0.0.0.0:14550"
     sleep 1
@@ -114,51 +106,24 @@ else
   exit 1
 fi
 
-# Check if submodules need to be cloned.
-check-and-reinit-submodules
+# Build the docker image.
+docker build -t uasatucla/px4-simulator docker
 
-# Set root path of the repository volume on the host machine.
-# Note: If docker is called within another docker instance & is trying to start
-#       the UAS@UCLA docker environment, the root will need to be set to the
-#       path that is used by wherever dockerd is running.
-ROOT_PATH=$(git rev-parse --show-superproject-working-tree)
-if [ -z "$ROOT_PATH" ]
-then
-  ROOT_PATH=$(pwd)
-fi
-
-if [ ! -z $HOST_ROOT_SEARCH ] && [ ! -z $HOST_ROOT_REPLACE ]
-then
-  # Need to use path of the host container running dockerd.
-  ROOT_PATH=${ROOT_PATH/$HOST_ROOT_SEARCH/$HOST_ROOT_REPLACE}
-fi
-
-CURRENT_DIRECTORY=$(pwd)
-PWD_LOC=$(python -c "import os.path; \
-  print(os.path.relpath(\"$CURRENT_DIRECTORY\", \"$ROOT_PATH\"))"\
-)
-
-# Build and run the docker image. Adjust file permissions of the docker user to
-# match the host.
-docker build -t uas-at-ucla_px4-simulator docker
+# Run the docker image.
 docker run                                                                     \
   -it                                                                          \
   --rm                                                                         \
-  -v $ROOT_PATH:/home/user/repo                                                \
-  --net host                                                                   \
+  -p 14570:14570/udp                                                           \
+  -p 14580:14580/udp                                                           \
+  -p 5760:5760/tcp                                                             \
   -e DISPLAY=:0                                                                \
   -v /tmp/.X11-unix:/tmp/.X11-unix:ro                                          \
-  --name uas-at-ucla_px4-simulator                                             \
-  uas-at-ucla_px4-simulator                                                    \
+  --name uasatucla_px4-simulator                                               \
+  uasatucla/px4-simulator                                                      \
   bash -c "
   set -e
-  cd /home/user/repo
-  HOST_USER=\$(stat -c '%u' .)
-  HOST_GROUP=\$(stat -c '%u' .)
-  getent group \$HOST_USER || groupadd -g \$HOST_GROUP host_group
-  usermod -u \$HOST_USER -g \$HOST_GROUP user
-  cd $PWD_LOC/Firmware
+  cd ./Firmware
   export PX4_HOME_LAT=$LATITUDE
   export PX4_HOME_LON=$LONGITUDE
   export PX4_HOME_ALT=$ALTITUDE
-  sudo -u user -H sh -c \"$MAKE_CMD\""
+  sh -c \"$RUN_CMD\""
